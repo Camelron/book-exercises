1d73d5ae1a14d9b754e6ac9882861415c7988b2f
Camelron <cam.thebaird@gmail.com>
2019-01-31 20:59:46 -0800

    Finish exercises chap12

--- chapter-11-exercises/exercise-5/exercise.R
+++ chapter-11-exercises/exercise-5/exercise.R
@@ -11,26 +11,34 @@ View(flights)
 # Save this as a data frame `dep_delay_by_month`
 # Hint: you'll have to perform a grouping operation then summarizing your data
 
- #I dont really know what to do here
-delayMonth$month <- c(1:12)
-delayMonth$delay <- 
+dep_delay_by_month <- flights %>% 
+  group_by(month) %>% 
+  filter(!is.na(dep_delay)) %>% 
+  summarize(avg_delay = mean(dep_delay))
 # Which month had the greatest average departure delay?
 max(dep_delay_by_month)
 
 # If your above data frame contains just two columns (e.g., "month", and "delay"
 # in that order), you can create a scatterplot by passing that data frame to the
 # `plot()` function
-plot(delayMonth)
+plot(dep_delay_by_month)
 
 # To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation then summarize your data
 # You can use the `head()` function to view just the first few rows
 
-#sort of lost here
+arr_delay_by_dest <- flights %>% 
+  group_by(dest) %>% 
+  filter(!is.na(arr_delay)) %>% 
+  summarize(avg_delay = mean(arr_delay))
 
 # You can look up these airports in the `airports` data frame!
-View(airports)
+View(arr_delay_by_dest)
 
 # Which city was flown to with the highest average speed?
 
-#sort of lost here
+speed_by_dest <- flights %>% 
+  group_by(dest) %>% 
+  filter(!is.na(distance / air_time)) %>% 
+  summarize(avg_speed = mean(distance / air_time))
+View(speed_by_dest)
--- chapter-12-exercises/exercise-1/exercise.R
+++ chapter-12-exercises/exercise-1/exercise.R
@@ -1,33 +1,41 @@
 # Exercise 1: analyzing avocado sales with the `tidyr` package
 
 # Load necessary packages (`tidyr`, `dplyr`, and `ggplot2`)
-
+install.packages("tidyr")
+install.packages("ggplot2")
+library("dplyr")
+library("tidyr")
+library("ggplot2")
 
 # Set your working directory using the RStudio menu:
 # Session > Set Working Directory > To Source File Location
 
 # Load the `data/avocado.csv` file into a variable `avocados`
 # Make sure strings are *not* read in as factors
-
-
+avocados <- read.csv("data/avocado.csv", stringsAsFactors = FALSE)
+View(avocados)
 # To tell R to treat the `Date` column as a date (not just a string)
 # Redefine that column as a date using the `as.Date()` function
 # (hint: use the `mutate` function)
-
+avocados <- mutate(avocados, Date = as.Date(Date))
 
 # The file had some uninformative column names, so rename these columns:
 # `X4046` to `small_haas`
 # `X4225` to `large_haas`
 # `X4770` to `xlarge_haas`
-
+avocados <- mutate(avocados, small_haas = X4046)
+avocados <- mutate(avocados, large_haas = X4225)
+avocados <- mutate(avocados, xlarge_haas = X4770)
+avocados <- select(avocados, -c(X4046, X4225, X4770))
 
 # The data only has sales for haas avocados. Create a new column `other_avos`
 # that is the Total.Volume minus all haas avocados (small, large, xlarge)
-
+avocados <- mutate(avocados, other_avos = Total.Volume - small_haas - large_haas - xlarge_haas)
 
 # To perform analysis by avocado size, create a dataframe `by_size` that has
 # only `Date`, `other_avos`, `small_haas`, `large_haas`, `xlarge_haas`
-
+by_size <- avocados %>% 
+  select(Date, other_avos, small_haas, large_haas, xlarge_haas)
 
 # In order to visualize this data, it needs to be reshaped. The four columns
 # `other_avos`, `small_haas`, `large_haas`, `xlarge_haas` need to be 
@@ -36,8 +44,8 @@
 # `volume`. Create a new dataframe `size_gathered` by passing the `by_size` 
 # data frame to the `gather()` function. `size_gathered` will only have 3 
 # columns: `Date`, `size`, and `volume`.
-
-
+size_gathered <- gather(by_size, key = size, value = volume) #I don't understand the textbook's explanation of what spread/gather do or how they work.
+View(size_gathered)
 # Using `size_gathered`, compute the average sales volume of each size 
 # (hint, first `group_by` size, then compute using `summarize`)
 
@@ -52,12 +60,15 @@ ggplot(size_gathered) +
 # Create a new data frame `by_type` by grouping the `avocados` dataframe by
 # `Date` and `type`, and calculating the sum of the `Total.Volume` for that type
 # in that week (resulting in a data frame with 2 rows per week).
-
-
+by_type <- group_by(avocados, Date, type) %>% 
+  summarise(sum = sum(Total.Volume))
+print(by_type)
 # To make a (visual) comparison of conventional versus organic sales, you 
 # need to **spread** out the `type` column into two different columns. Create a 
 # new data frame `by_type_wide` by passing the `by_type` data frame to 
 # the `spread()` function!
+by_type_wide <- spread(by_type, key = type, value = sum)
+View(by_type_wide)
 
 
 # Now you can create a scatterplot comparing conventional to organic sales!
